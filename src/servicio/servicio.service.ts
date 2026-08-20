38c9285df3c6c47d52c0ce8d3acddfb08b188232
Ivan-M <sirius.ivo@gmail.com>
2026-08-20 13:01:21 -0300

    feat: implement ServicioService with CRUD methods and unit tests

---
+++ src/servicio/servicio.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ServicioService {
+  constructor(private prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.servicio.findMany();
+  }
+  findOne(id: string) {
+    return this.prisma.servicio.findUnique({
+      where: { id },
+    });
+  }
+  create(data: {
+    nombre: string;
+    monto: number;
+    vencimiento: string;
+    empresaId: string;
+  }) {
+    return this.prisma.servicio.create({ data });
+  }
+  update(
+    id: string,
+    data: {
+      nombre: string;
+      monto: number;
+      vencimiento: string;
+      empresaId: string;
+    },
+  ) {
+    return this.prisma.servicio.update({
+      where: { id },
+      data,
+    });
+  }
+  delete(id: string) {
+    return this.prisma.servicio.delete({
+      where: { id },
+    });
+  }
+}
