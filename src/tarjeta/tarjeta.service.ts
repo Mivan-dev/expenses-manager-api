c40eee12865efbe8f6fabc05912eddc4c629d9bb
Ivan-M <sirius.ivo@gmail.com>
2026-08-20 02:47:44 -0300

    feat: implement TarjetaService with CRUD operations

--- src/tarjeta/tarjeta.service.ts
+++ src/tarjeta/tarjeta.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TarjetaService {}
+export class TarjetaService {
+    constructor(private prisma: PrismaService){}
+
+    findAll(){
+        return this.prisma.tarjeta.findMany({
+            include: { cuotas: true }
+        })
+    }
+
+    findOne(id: string){
+        return this.prisma.tarjeta.findUnique({
+            where: { id },
+            include: { cuotas: true }
+        })
+    }
+
+    create(data: {nombre: string, monto: number, vencimiento: string, empresaId: string}){
+        return this.prisma.tarjeta.create({data})
+    }
+
+    update(id: string, data: {nombre: string, monto: number, vencimiento: string, empresaId: string}){
+        return this.prisma.tarjeta.update({
+            where: { id },
+            data
+        })
+    }
+
+    delete(id: string){
+        return this.prisma.tarjeta.delete({
+            where: { id }
+        })
+    }
+}
