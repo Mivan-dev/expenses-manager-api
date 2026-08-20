e07b7997ada8189eeaf72a40ed486d4403f8723a
Ivan-M <sirius.ivo@gmail.com>
2026-08-20 03:20:46 -0300

    feat: implement CuotaService with create, update, and delete methods

--- src/cuota/cuota.service.ts
+++ src/cuota/cuota.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CuotaService {}
+export class CuotaService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: {
+    nombre: string;
+    cuotaActual: number;
+    cuotaBase: number;
+    cuotaTotal: number;
+    monto: number;
+    fechaCarga: string;
+    tarjetaId: string;
+  }) {
+    return this.prisma.cuota.create({ data });
+  }
+
+  update(
+    id: string,
+    data: {
+      nombre: string;
+      cuotaActual: number;
+      cuotaBase: number;
+      cuotaTotal: number;
+      monto: number;
+      fechaCarga: string;
+      tarjetaId: string;
+    },
+  ){
+    return this.prisma.cuota.update({
+        where: { id },
+        data
+    })
+  }
+
+  delete(id: string) {
+    return this.prisma.cuota.delete({
+        where: { id }
+    })
+  }
+}
