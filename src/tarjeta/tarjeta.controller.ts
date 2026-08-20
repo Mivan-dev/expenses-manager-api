28e53e202afd171585453b412f34c22debe99b81
Ivan-M <sirius.ivo@gmail.com>
2026-08-20 02:47:52 -0300

    feat: implement TarjetaController with CRUD endpoints

--- src/tarjeta/tarjeta.controller.ts
+++ src/tarjeta/tarjeta.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TarjetaService } from './tarjeta.service';
 
 @Controller('tarjeta')
-export class TarjetaController {}
+export class TarjetaController {
+    constructor(private tarjetaService: TarjetaService){}
+
+    @Get()
+    findAll(){
+        return this.tarjetaService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return this.tarjetaService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() data: {nombre: string, monto: number, vencimiento: string, empresaId: string}){
+        return this.tarjetaService.create(data)
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() data: {nombre: string, monto: number, vencimiento: string, empresaId: string}){
+        return this.tarjetaService.update(id, data)
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string){
+        return this.tarjetaService.delete(id)
+    }
+}
