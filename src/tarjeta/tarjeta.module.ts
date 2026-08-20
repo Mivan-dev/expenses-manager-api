7d3703ca50d64630982002c43e7b72c0df36cf06
Ivan-M <sirius.ivo@gmail.com>
2026-08-20 02:47:57 -0300

    feat: add PrismaModule to TarjetaModule imports

--- src/tarjeta/tarjeta.module.ts
+++ src/tarjeta/tarjeta.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TarjetaService } from './tarjeta.service';
 import { TarjetaController } from './tarjeta.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [TarjetaService],
   controllers: [TarjetaController]
 })
