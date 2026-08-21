789b6e86927ae3bf959760826f7f096c70b00e54
Ivan-M <sirius.ivo@gmail.com>
2026-08-21 20:17:27 -0300

    feat: implement UsuarioService with create and findByEmail methods

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsuarioService {}
+export class UsuarioService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: { nombre: string; email: string; password: string }) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    return this.prisma.usuario.create({
+      data: { ...data, password: hashedPassword },
+    });
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.usuario.findUnique({
+      where: { email },
+    });
+  }
+}
