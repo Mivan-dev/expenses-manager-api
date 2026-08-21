588f1ef0a7a0dd86e6f96b88c12db74cfe3e18ec
Ivan-M <sirius.ivo@gmail.com>
2026-08-21 20:39:30 -0300

    feat: implement AuthService with login method and integrate JwtService and UsuarioService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsuarioService } from 'src/usuario/usuario.service';
+import { JwtService } from '@nestjs/jwt'
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usuarioService: UsuarioService,
+    private jwtService: JwtService
+  ) {}
+
+  async login(email: string, password: string){
+    const usuarioLogin = await this.usuarioService.findByEmail(email);
+    if(usuarioLogin){
+        const passwordValido = await bcrypt.compare(password, usuarioLogin.password)
+        if( passwordValido){
+            return this.jwtService.sign({sub: usuarioLogin.id, email: usuarioLogin.email});
+        } else {
+            throw new UnauthorizedException('Credenciales inválidas')
+        }
+    } else {
+        throw new UnauthorizedException('Credenciales inválidas')
+    }
+  }
+}
