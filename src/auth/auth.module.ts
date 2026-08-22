d409df9d7c09ac3c6b53ed1168bd0c8ae3b5cfe0
Ivan-M <sirius.ivo@gmail.com>
2026-08-22 04:39:35 -0300

    feat: update AuthModule to include PassportModule and JwtStrategy; export UsuarioService from UsuarioModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsuarioModule } from 'src/usuario/usuario.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
+    UsuarioModule,
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' },
-    }),
-    UsuarioModule,
+    })
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
